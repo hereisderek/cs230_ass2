bbfb356e0cdcf4c89c4d7fb8e388aee98b2e432a
derek ju <1and1get2@gmail.com>
2013-04-19 14:33:22 +1200

    part 2, very doubtly done some crap, a little bit clean job

--- Asst2/Test/qwta/QuizTest.java
+++ Asst2/Test/qwta/QuizTest.java
@@ -10,7 +10,7 @@ import java.util.Vector;
 public class QuizTest {
 
 	private Quiz quiz1, quizMax, quizAvg, quizLast;
-	private Vector<Integer> vector1 = new Vector<Integer>();
+	private Vector<Integer> vector1;
 	private Course course = new Course("course_name", new Instructor(
 			"Instructor of a course"));
 	private Scoresheet scoresheet; 
@@ -20,13 +20,19 @@ public class QuizTest {
 		quizMax = new Quiz("quizMax", course, 5, SummaryStatistic.MAX_SCORE);
 		quizAvg = new Quiz("quizAvg", course, 5, SummaryStatistic.AVG_SCORE);
 		quizLast = new Quiz("quizLast", course, 5, SummaryStatistic.LAST_SCORE);
+		vector1 = new Vector<Integer>();
 		vector1.add(10);
 		vector1.add(20);
 		vector1.add(30);
 		vector1.add(20);
 		scoresheet = new Scoresheet(quizAvg, new Marksheet(new Student("student1"), course));
 	}
-
+	
+	/**
+	 * Question 1
+	 * 
+	 * 22 cases, omg
+	 * */
 	// coverage of the three methods for computing a SummaryStatistic on a
 	// vector of integer scores
 	@Test
@@ -46,7 +52,7 @@ public class QuizTest {
 
 	// coverage of different lengths of the score vector
 	@Test
-	public void testSummariseScoresMoreElement() {
+	public void testSummariseScoresTooManyElementsAvg() {
 		// create a lot of element and add into vector1
 		// UNVERIFIED_BUG if NUMBERS_OF_ELEMENT becomes large enough
 		int NUMBERS_OF_ELEMENT = 1000000;
@@ -62,62 +68,139 @@ public class QuizTest {
 
 	// 1 c,d (none)integer values within the score vector
 	@Test
-	public void testSummariseScoresMoreMinusElement() {
+	public void testSummariseScoresMinusNumberAvg() {
 		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.AVG_SCORE);
 		vector1.add((-30));
-		// cause a error: JAVA_TYPE_SAFETY_SUCCESSFUL_TEST (minus integer)
+		assertEquals((Integer) 20, quiz1.summariseScores(vector1));
+	}
+	@Test
+	public void testSummariseScoresMinusNumberMax() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.MAX_SCORE);
+		vector1.add((-30));
+		assertEquals((Integer) 30, quiz1.summariseScores(vector1));
+	}
+	@Test
+	public void testSummariseScoresMinusNumberLast() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.LAST_SCORE);
+		vector1.add((-30));
 		assertEquals((Integer) 20, quiz1.summariseScores(vector1));
 	}
 
 	// none-interger
 	@Test
-	public void testSummariseScoresMoreNoneIntegerElement() {
+	public void testSummariseScoresMoreNoneIntegerElementAvg() {
 		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.AVG_SCORE);
 		Vector vector2 = new Vector(vector1);
 		vector2.add("hello");
+		assertEquals((Integer) 20, quiz1.summariseScores(vector2));
+	}
+	@Test
+	public void testSummariseScoresMoreNoneIntegerElementMax() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.MAX_SCORE);
+		Vector vector2 = new Vector(vector1);
+		vector2.add("hello");
 		// cause a error: JAVA_TYPE_SAFETY_SUCCESSFUL_TEST UNVERIFIED_BUG
+		assertEquals((Integer) 30, quiz1.summariseScores(vector2));
+	}
+	@Test
+	public void testSummariseScoresMoreNoneIntegerElementLast() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.LAST_SCORE);
+		Vector vector2 = new Vector(vector1);
+		vector2.add(' ');
+		assertEquals((Integer) 20, quiz1.summariseScores(vector2));
+	}
+
+	// char
+	@Test
+	public void testSummariseScoresCharAvg() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.AVG_SCORE);
+		Vector vector2 = new Vector(vector1);
+		vector2.add("hello");
+		assertEquals((Integer) 20, quiz1.summariseScores(vector2));
+	}
+	@Test
+	public void testSummariseScoresCharMax() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.MAX_SCORE);
+		Vector vector2 = new Vector(vector1);
+		vector2.add('a');
+		// cause a error: JAVA_TYPE_SAFETY_SUCCESSFUL_TEST UNVERIFIED_BUG
+		assertEquals((Integer) 30, quiz1.summariseScores(vector2));
+	}
+	@Test
+	public void testSummariseScoresCharLast() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.LAST_SCORE);
+		Vector vector2 = new Vector(vector1);
+		vector2.add('\n');
 		assertEquals((Integer) 20, quiz1.summariseScores(vector2));
 	}
 
 	// none-interger, float number
 	@Test
-	public void testSummariseScoresMoreNoneIntegerElementFloat() {
+	public void testSummariseScoresFloatAvg() {
 		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.AVG_SCORE);
 		Vector vector2 = new Vector();
 		vector2.add(30);
 		vector2.add(2.3);
-		// cause a error: JAVA_TYPE_SAFETY_SUCCESSFUL_TEST UNVERIFIED_BUG
+		assertEquals((Integer) 30, quiz1.summariseScores(vector2));
+	}
+	@Test
+	public void testSummariseScoresFloatMax() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.MAX_SCORE);
+		Vector vector2 = new Vector();
+		vector2.add(30);
+		vector2.add(2.3);
+		assertEquals((Integer) 30, quiz1.summariseScores(vector2));
+	}
+	@Test
+	public void testSummariseScoresFloatLast() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.LAST_SCORE);
+		Vector vector2 = new Vector();
+		vector2.add(30);
+		vector2.add(2.3);
 		assertEquals((Integer) 30, quiz1.summariseScores(vector2));
 	}
 
 	// none-interger, float number
 	@Test
-	public void testSummariseScoresNull() {
+	public void testSummariseScoresNullAvg() {
 		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.AVG_SCORE);
-		// Vector vector2 = new Vector();
-		// vector2.add(30);
 		vector1.add(null);
-		// cause a error: JAVA_TYPE_SAFETY_SUCCESSFUL_TEST UNVERIFIED_BUG
 		assertEquals((Integer) 20, quiz1.summariseScores(vector1));
 	}
+	@Test
+	public void testSummariseScoresNullMax() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.MAX_SCORE);
+		vector1.add(null);
+		assertEquals((Integer) 30, quiz1.summariseScores(vector1));
+	}
+	@Test
+	public void testSummariseScoresNull() {
+		quiz1 = new Quiz("quiz1", course, 5, SummaryStatistic.LAST_SCORE);
+		vector1.add(null);
+		assertEquals((Integer) 20, quiz1.summariseScores(vector1));
+	}
+	/**
+	 * Question 2
+	 * 
+	 * */
 
-	// @Test
-	// public void testSummariseScoresAvg() {
-	// //assertEquals("testSummariseScores 1: ", 100,
-	// quiz1.summariseScores(vector1));
-	// assertEquals((Integer)20, quiz1.summariseScores(vector1));
-	// }
-
-	// test 3 attempts
+	@Test 
+	public void testSitQuizAttemptZeroTimes(){
+		// not one time sitQuiz() executed
+		scoresheet.reportOnScoresForStudent();
+		scoresheet.reportScoresToInstructor();
+	}
+	
 	@Test
 	public void testSitQuizAttemptThreeTimes(){
-//		Scoresheet scoresheet= new Scoresheet(quizAvg, new Marksheet(new Student("student1"), course));
+		// three times, within maxAttempts
 		assertEquals(0, quizAvg.sitQuiz(scoresheet));
 		assertEquals(0, quizAvg.sitQuiz(scoresheet));
 		assertEquals(0, quizAvg.sitQuiz(scoresheet));
-//		scoresheet.reportOnScoresForStudent();
-//		scoresheet.reportScoresToInstructor();
+		scoresheet.reportOnScoresForStudent();
+		scoresheet.reportScoresToInstructor();
 	}
+	
 	// BUGFIX
 	// test more attempt than allowed 
 	@Test
@@ -130,8 +213,8 @@ public class QuizTest {
 		assertEquals(0, quizAvg.sitQuiz(scoresheet));
 		assertEquals(0, quizAvg.sitQuiz(scoresheet));
 		assertEquals(0, quizAvg.sitQuiz(scoresheet));
-//		scoresheet.reportOnScoresForStudent();
-//		scoresheet.reportScoresToInstructor();
+		scoresheet.reportOnScoresForStudent();
+		scoresheet.reportScoresToInstructor();
 	}
 
 	//@SuppressWarnings("deprecation")
@@ -141,8 +224,6 @@ public class QuizTest {
 		double maxMarks = 0.6;	//scale a student’s summarised score
 		quizAvg.maxScore = maxScore;
 		quizAvg.maxMarks = maxMarks;
-		//assertEquals(10, quizAvg.scaledMark(vector1));
-		System.out.println(quizAvg.scaledMark(vector1));
 		assertTrue("testScaledMark1", quizAvg.scaledMark(vector1) == 1.2);
 		//assertEquals(quizAvg.summariseScores(vector1) / maxScore * maxMarks, quizAvg.scaledMark(vector1));
 	}
